77037f786ac0e417a40f5b66b874ed59380d8672
Maheshcloud <59549736+Maheshcloud@users.noreply.github.com>
2025-05-23 15:33:08 +0530

    Update Naukri.py

--- Naukri.py
+++ Naukri.py
@@ -7,8 +7,8 @@ import time
 # Initialize Chrome driver instance
 driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
 
-username = ${{ secrets.USERNAME }}
-password = ${{ secrets.PASSWORD }}
+username = "${{ secrets.USERNAME }}"
+password = "${{ secrets.PASSWORD }}"
 
 
 # Navigate to Naukri login page
